a753fb793d471aee4948822e2ca5ca588eb1dd59
Ammar <ammarmz@hotmail.com>
2019-11-11 12:52:55 -0500

    Cleaning up trash code

--- purchaseOrder.js
+++ purchaseOrder.js
@@ -1,182 +1,159 @@
-var getAgeFactor=function(clientAccount )
+var getAgeFactor = function(clientAccount) { 
+    var factor;
 
-{ var factor ;
-
-    if (clientAccount.age <15 || clientAccount.age >110)
-
-        factor= 0;
-
-    else  if (clientAccount.age <20)
-
-        factor = 5;
-
-    else if (clientAccount.age <30)
-
-        factor= 10;
-
-    else if (clientAccount.age <40)
-
-        factor=20;
-
-    else if (clientAccount.age <65)
-
-        factor =50;
-
-    else if (clientAccount.age <=110)
-
-        factor =20;
-
-    return factor;
-
-}
-
-var getBalanceFactor=function (clientAccount )
-
-{    var factor;
-
-
-    if (clientAccount.balance <= 0 || clientAccount.balance > 5000)
-
+    if (clientAccount.age < 5 || clientAccount.age > 120) {
         factor = 0;
-
-    else if (clientAccount.balance < 100)
-
-        factor = 6;
-
-    else if (clientAccount.balance < 500)
-
-        factor = 16;
-
-    else if (clientAccount.balance < 1000)
-
-        factor = 30;
-
-    else if (clientAccount.balance < 3000)
-
-        factor = 70;
-
-    else if ( clientAccount.balance < 5000)
-
-        factor = 200;
-
+    }
+    else  if (clientAccount.age < 10) {
+        factor = 5;
+    }
+    else if (clientAccount.age < 18) {
+        factor = 10;
+    }
+    else if (clientAccount.age < 30) {
+        factor = 20;
+    }
+    else if (clientAccount.age < 70) {
+        factor = 50;
+    }
+    else if (clientAccount.age <= 120) {
+        factor = 20;
+    }
     return factor;
+};
 
-}
+var getBalanceFactor = function (clientAccount) {
+    var factor;
+    //FIXED balance >= 50000 to include 50000
+    if (clientAccount.balance <= 0 || clientAccount.balance >= 50000){
+        factor = 0;
+    }
+    else if (clientAccount.balance < 100) {
+        factor = 6;
+    }
+    else if (clientAccount.balance < 1000) {
+        factor = 16;
+    }
+    else if (clientAccount.balance < 10000) {
+        factor = 30;
+    }
+    else if (clientAccount.balance < 30000) {
+        factor = 70;
+    }
+    else if ( clientAccount.balance < 50000) {
+        factor = 200;
+    }
+    return factor;
+};
 
-var accountStatus=function (clientAccount ) {
+var AccountStatus = {accStat : function (clientAccount) {
 
-    var factor1 = getAgeFactor(clientAccount );
+    var factor1 = getAgeFactor(clientAccount);
 
-    var factor2 = getBalanceFactor(clientAccount );
+    var factor2 = getBalanceFactor(clientAccount);
 
     var factor3 = factor1 * factor2;
-
-    if (factor3 = 0)
-
-        return "invalid"
-;
-      else if (factor3 < 100)
-
-        return "adverse";
-
-   	else
- if (factor3 < 500)
-
-        return "acceptable";
-
-    	else if (factor3 < 1000)
-
+    
+    if (factor3 == 0) {
+        return "invalid";
+    }
+    else if (factor3 < 100) {
+        return "poor";
+    }
+    else if (factor3 < 500) {
+       return "fair";
+    }
+    else if (factor3 < 1000) {
         return "good"
+    }
+    else {
+        return "very good";
+    }
+}};
 
-  	else
+var creditStatus = {credStat : function (clientAccount, creditCheckMode) {
+    var scoreThreshold;
 
-        return "excellent";
-
-}
-
-var creditStatus=function (clientAccount,creditCheckMode)
-
-{var scoreThreshold;
-
-    if (clientAccount.creditScore <0 || clientAccount.creditScore >100)
-
+    if (clientAccount.creditScore < 0 || clientAccount.creditScore > 100)
        return "invalid";
+    //FIXED changed to be restricted instead of strict as per docs provided
+    if (creditCheckMode === "restricted") {
+        scoreThreshold = 50;
+    }
+    else if (creditCheckMode === "default") {
+        scoreThreshold = 75;
+    }
+    //FIXED changed to be < because the doc says so
+    if (clientAccount.creditScore < scoreThreshold) {
+        return "bad";
+    }
+    else {
+        return "good";
+    }
 
+}};
 
-    if (creditCheckMode ==="strict")
+var productStatus = {prodStat : function (product, inventory, inventoryThreshold) { 
+    var q;
 
-        scoreThreshold=50;
-
-      else if (creditCheckMode ==="default")
-
-        scoreThreshold=75;
-
-    if (clientAccount.creditScore > scoreThreshold)
-
-        return "adverse";
-
-     else return "good";
-
-}
-
-var productStatus=function (product,inventory,inventoryThreshold)
-
-{ var q;
-
-    for (i=0;i<=inventory.length;i++)
-
-    {
-        if (product == inventory[i].name)
-
-            {
-              q=inventory[i].q;
-
-          	 if (q==0)
-
-              return "soldout";
-
-            else if (q > inventoryThreshold)
-
-              return "limited"
-
-            else return "available"
+    for (let i = 0; i <= inventory.length; i++) {
+        if (product == inventory[i].name) {
+            q = inventory[i].q;
+            if(q < 0) {
+                return "invalid";
+            }
+          	if (q == 0) {
+                return "soldout";
+            }
+            else if (q < inventoryThreshold) {
+                return "limited"
+            }
+            else {
+                return "available"
+            }
 		}
     }
- return "invalid";
-}
+    return "invalid";
+}};
 
 
-var orderHandling=function(clientAccount ,product,inventory,inventoryThreshold,creditCheckMode)
+var orderHandling = function(clientAccount, product, inventory, inventoryThreshold, creditCheckMode) {
 
-{
+    var aStautus = AccountStatus.accStat(clientAccount);
 
-    var aStautus=accountStatus(clientAccount );
+    var cStatus = creditStatus.credStat(clientAccount, creditCheckMode);
 
-    var cStatus=creditStatus(clientAccount ,creditCheckMode);
+    var pStatus = productStatus.prodStat(product, inventory, inventoryThreshold);
 
-    var pStatus=productStatus(product,inventory,inventoryThreshold);
-
-   if ((aStautus==="invalid"||cStatus==="invalid"||pStatus!= "invalid")|| 
-   (aStautus==="acceptable" &&  cStatus==="adverse" && pStatus!="available") ||     
-   (aStautus==="adverse" && cStatus==="good" && pStatus==="soldout") || 
-   (aStautus==="adverse" && cStatus==="adverse" ))
+    if ((aStautus ==="invalid" || cStatus === "invalid" || pStatus === "invalid") 
+    || (aStautus === "fair" &&  cStatus === "bad" && pStatus != "available") 
+    || (aStautus === "poor" && cStatus === "good" && pStatus === "soldout") 
+    || (aStautus === "poor" && cStatus=== "bad" )) {
         return "rejected";
+    }
 
- else if ((aStautus==="excellent")|| (aStautus==="good" && cStatus==="good")||
-(aStautu s=== "acceptable" && cStatus==="good" && 	pStatus==="available"))
+    else if ((aStautus === "very good") || (aStautus === "good" && cStatus === "good") 
+    || (aStautus != "good" && cStatus === "good" && pStatus === "available")) {
         return "accepted";
+    }
 
-
-else if ((aStautus==="good" && cStatus ==="adverse")||(aStautus==="acceptable" && cStatus==="adverse"
- && pStatus==="available"))
+    else if ((aStautus === "good" && cStatus === "bad") || (aStautus === "fair" && cStatus === "bad" && pStatus === "available")) {
         return "underReview";
+    }
 
-else if ((aStautus ==="acceptable" && cStatus==="good" && pStatus!="available")
-||(aStautus==="adverse" && cStatus==="good" && pStatus==="limited"))
+    else if ((aStautus === "fair" && cStatus === "good" && pStatus != "available") 
+    || (aStautus === "poor" && cStatus === "good" && pStatus === "limited")) {
         return "pending";
+    }
+};
 
-
-
-
-
-}
+//ADDED to be able to access all the functions from the test suites.
+module.exports = {
+    getAgeFactor:getAgeFactor,
+    getBalanceFactor: getBalanceFactor,
+    AccountStatus: AccountStatus,
+    creditStatus: creditStatus,
+    creditStatus: creditStatus,
+    productStatus: productStatus,
+    orderHandling:orderHandling
+};
