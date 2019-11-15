bcd1733a33bdf787994c7eac00f37f80eee6dd0a
Ammar <ammarmz@hotmail.com>
2019-11-15 12:02:30 -0500

    Added boundary testing to creditStatusTest.js

--- test/creditStatusTest.js
+++ test/creditStatusTest.js
@@ -8,6 +8,10 @@ const good = 'good';
 const ccmRestricted = 'restricted';
 const ccmDefault = 'default';
 
+
+let creditCheckModes = [
+	ccmRestricted, ccmDefault,
+]
 describe('Credit Status Tests', () => {
 	/**
 	 * Decision Table Tests
@@ -20,43 +24,71 @@ describe('Credit Status Tests', () => {
 
 		// setting up variable ranges  
 		let creditScore = [
-            {start: -100, end : 0}, 
-            {start: 0, end: 50},
-            {start: 50, end: 75},
-            {start: 75, end: 101},
-            {start: 101, end: 200},
-        ];
+			{ start: -100, end: 0 },
+			{ start: 0, end: 50 },
+			{ start: 50, end: 75 },
+			{ start: 75, end: 101 },
+			{ start: 101, end: 200 },
+		];
 
-        let creditCheckModes = [
-            ccmRestricted, ccmDefault,
-        ]
+		// manually calculated the return values
+		let returns = [
+			[invalid, invalid],
+			[adverse, adverse],
+			[good, adverse],
+			[good, good],
+			[invalid, invalid],
+		];
+		// tc is to number the test output 
+		let tc = 1;
+		for (let i = 0; i < creditScore.length; i++) {
 
-        // manually calculated the return values
-        let returns = [
-            [invalid, invalid],
-            [adverse, adverse],
-            [good, adverse],
-            [good, good],
-            [invalid, invalid],
-        ];
-        // tc is to number the test output 
-        let tc = 1;
-        for (let i = 0; i < creditScore.length; i++) {
+			let currCredit = getRandInRange(creditScore[i].start, creditScore[i].end);
+			tc = 1;
 
-            let currCredit = getRandInRange(creditScore[i].start, creditScore[i].end);
-            tc = 1;
-
-            for (let j = 0; j < creditCheckModes.length; j++) {
-                it(`Decision Table Test ${(i+1) + '-' + tc++}: Return ${returns[i][j]} for credit score = ${currCredit} and credit check mode = ${creditCheckModes[j]}`, 
-                 () => {
-                    let input = {
-                        creditScore: currCredit,
-                    };
-                    assert.equal(purchaseOrder.creditStatus(input, creditCheckModes[j]), returns[i][j]);
-                });
-            }
-        }	
+			for (let j = 0; j < creditCheckModes.length; j++) {
+				it(`Decision Table Test ${(i + 1) + '-' + tc++}: Return ${returns[i][j]} for credit score = ${currCredit} and credit check mode = ${creditCheckModes[j]}`,
+					() => {
+						let input = {
+							creditScore: currCredit,
+						};
+						assert.equal(purchaseOrder.creditStatus(input, creditCheckModes[j]), returns[i][j]);
+					});
+			}
+		}
 	});
+
+	let creditScoreBoundaries = [
+		-1, 0, 1, 99, 100, 101
+	];
+
+	let BCreturns = [
+		[invalid, invalid],
+		[adverse, adverse],
+		[adverse, adverse],
+		[good, good],
+		[good, good],
+		[invalid, invalid],	
+	];
+
+
+	describe('Boundary Value Tests', () => {
+		for (let i = 0; i < creditScoreBoundaries.length; i++) {
+
+			let currCredit = creditScoreBoundaries[i];
+			for (let j = 0; j < creditCheckModes.length; j++) {
+
+				it(`Boundary Test ${(i + 1)}: Return ${BCreturns[i][j]} for credit score = ${currCredit} and credit check mode = ${creditCheckModes[j]}`, () => {
+					let input = {
+						creditScore: currCredit,
+					};
+					assert.equal(purchaseOrder.creditStatus(input, creditCheckModes[j]), BCreturns[i][j]);
+				});
+
+			}
+		}
+	});
+
 });
 
 /**
@@ -66,5 +98,5 @@ describe('Credit Status Tests', () => {
  *  Gets a random value in within the range instead of hard coding a value from the range
  */
 function getRandInRange(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+	return Math.floor(Math.random() * (max - min) + min);
 }
